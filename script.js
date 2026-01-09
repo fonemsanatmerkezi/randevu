c9caf9c8984e33f23ad057f5aef2709c6a221aa0
fonemsanatmerkezi <fonemsanatmerkezi@gmail.com>
2026-01-10 01:27:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,20 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzYwsHWw53vh1ApVp7eUqV9mpXrMDgG904cd5A2sYRGD97-74mnm0AsczQakNJgPT_Bvw/exec";
-
-let tumRandevular = [];
-
-function giris() {
-  document.querySelector(".login").classList.add("hidden");
-  document.querySelector(".panel").classList.remove("hidden");
-  yukle();
-}
-
-function yukle() {
-  fetch(API_URL)
-    .then(r => r.json())
-    .then(data => {
-      oda.innerHTML = "";
-      data.odalar.forEach(o => {
-        oda.innerHTML += `<option>${o}</option>`;
-      });
-      tumRandevular = data.randevular;
-      listele();
-    });
-}
+const API_URL = "APPS_SCRIPT_URL_BURAYA";
 
 function kaydet() {
-  if (!oda.value || !tarih.value || !saat.value) {
-    alert("Oda, gün ve saat seçilmeli");
-    return;
-  }
+  const data = {
+    adSoyad: document.getElementById("adSoyad").value,
+    telefon: document.getElementById("telefon").value,
+    gun: document.getElementById("gun").value,
+    saat: document.getElementById("saat").value,
+    kisiSayisi: document.getElementById("kisiSayisi").value,
+    oda: document.getElementById("oda").value,
+    notlar: document.getElementById("notlar").value
+  };
 
   fetch(API_URL, {
     method: "POST",
-    body: JSON.stringify({
-      sifre: sifre.value,
-      oda: oda.value,
-      ad: ad.value,
-      tarih: tarih.value,
-      saat: saat.value,
-      not: not.value
-    })
+    body: JSON.stringify(data)
   })
-  .then(r => r.json())
-  .then(d => {
-    if (d.durum === "dolu") {
-      alert("Bu oda bu saatte dolu");
-    }
-    yukle();
-  });
-}
-
-function listele() {
-  liste.innerHTML = "";
-  tumRandevular.forEach(r => {
-    liste.innerHTML += `
-      <li>
-        🏠 <b>${r[1]}</b><br>
-        👤 ${r[2]}<br>
-        📅 ${r[3]} ⏰ ${r[4]}<br>
-        📝 ${r[5] || "-"}
-      </li>
-    `;
-  });
+  .then(() => alert("Randevu kaydedildi"))
+  .catch(() => alert("Hata oluştu"));
 }
